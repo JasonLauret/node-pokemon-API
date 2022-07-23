a04f19379fdcd2915f789dfaf1552ef06ee3eec2
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-07-23 16:53:35 +0100

    Added a new 'Endpoint'(new path) '/api/pokemon/1'

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const port = 3000;
 
 app.get('/', (req,res) => res.send('Hello again, Express !'));
 
+app.get('/api/pokemon/1', (req,res) => res.send('Hello, Bulbizarre !'));
+
 app.listen(port,() => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
