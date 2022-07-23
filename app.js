567adfb25159db478b2c7c055e39fafd7dc02806
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-07-23 15:25:05 +0100

    Installing 'nodemon'

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => res.send('Hello, Express !'));
+app.get('/', (req,res) => res.send('Hello, Express 2 !'));
 
 app.listen(port,() => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
