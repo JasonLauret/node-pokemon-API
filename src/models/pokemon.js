4d20bb88ca6ff396a8903696f9caecf4f6831585
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-07-29 14:37:58 +0100

    Ajout des gestions des erreurs avec validate (min, max) dans 'src/models/pokemon' pour vérifier si l'utilisateur à entrer les bon types de données

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -9,24 +9,40 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: { msg: "Le nom ne peut pas être vide" },
-          notNull: { msg: "Le nom est une propriété requis" }
+          notEmpty: { msg: "Le nom ne peut pas être vide." },
+          notNull: { msg: "Le nom est une propriété requis." }
         }
       },
       hp: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
-          isInt: { msg: "Utilisez uniquement un nombre entier pour les points de vie" },
-          notNull: { msg: "Les points de vie sont une propriété requise" }
+          isInt: { msg: "Utilisez uniquement un nombre entier pour les points de vie." },
+          min: {
+            args:[0],
+            msg: 'Le nombre des points de vie doit être superieur ou égale à 0.'
+          },
+          max: {
+            args:[999],
+            msg: 'Le nombre des points de vie doit être inferieur ou égale à 999.'
+          },
+          notNull: { msg: "Les points de vie sont une propriété requise." }
         }
       },
       cp: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
-          isInt: { msg: "Utilisez uniquement un nombre entier pour les points de dégats" },
-          notNull: { msg: "Les points de dégats sont une propriété requise" }
+          isInt: { msg: "Utilisez uniquement un nombre entier pour les points de dégats." },
+          min: {
+            args:[0],
+            msg: 'Le nombre des points de dégats doit être superieur ou égale à 0.'
+          },
+          max: {
+            args:[99],
+            msg: 'Le nombre des points de dégats doit être inferieur ou égale à 99.'
+          },
+          notNull: { msg: "Les points de dégats sont une propriété requise." }
         }
       },
       picture: {
