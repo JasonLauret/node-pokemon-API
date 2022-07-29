dc6827e4aa9acfb74777eb952eaa8a77facd8632
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-07-29 14:21:14 +0100

    Ajout des gestions des erreurs avec validate dans 'src/models/pokemon' pour vérifier si l'utilisateur à entrer les bon types de données

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -7,23 +7,35 @@ module.exports = (sequelize, DataTypes) => {
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Le nom ne peut pas être vide" },
+          notNull: { msg: "Le nom est une propriété requis" }
+        }
       },
       hp: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
-          isInt: { msg: "Utilisez uniquement des nombre entiers pour les points de vie" },
-          notNull: { msg:"Les points de vie sont une propriété requise" }
+          isInt: { msg: "Utilisez uniquement un nombre entier pour les points de vie" },
+          notNull: { msg: "Les points de vie sont une propriété requise" }
         }
       },
       cp: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isInt: { msg: "Utilisez uniquement un nombre entier pour les points de dégats" },
+          notNull: { msg: "Les points de dégats sont une propriété requise" }
+        }
       },
       picture: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isUrl: { msg: "Utilisez uniquement une URL valide" },
+          notNull: { msg: "L'image est une propriété requise" }
+        }
       },
       types: {
         type: DataTypes.STRING,
