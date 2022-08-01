9b9bcf2f9a3456bc6fc954e75d81c9075899b674
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-08-01 12:18:31 +0100

    Modification de la requête avancée avec l'utilisation d'un opérateur Sequelize

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,10 +1,17 @@
 const { Pokemon } = require('../db/sequelize');
-  
+const { Op } = require('sequelize');
+
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     if(req.query.name){
       const name = req.query.name;
-      return Pokemon.findAll({ where:{ name: name} })
+      return Pokemon.findAll({ 
+        where:{ 
+          name: { // <- 'name' est la propriété du modèle pokémon
+            [Op.eq]: name // <- 'name' est le critère de la recherche
+          }
+        } 
+      })
       .then(pokemons => {
         const message = `Il y a ${pokemons.length} pokémon qui correspond au terme de recherche ${name}.`;
         res.json({ message, data: pokemons })
