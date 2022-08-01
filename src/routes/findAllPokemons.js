71739afbd3e92d7f675a7042f1736aba1825079e
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-08-01 12:10:13 +0100

    Ajout d'une fonctionnalité de recherhce (Requete avancée) dans src/routes/findAllPokemons

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -2,7 +2,15 @@ const { Pokemon } = require('../db/sequelize');
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name){
+      const name = req.query.name;
+      return Pokemon.findAll({ where:{ name: name} })
+      .then(pokemons => {
+        const message = `Il y a ${pokemons.length} pokémon qui correspond au terme de recherche ${name}.`;
+        res.json({ message, data: pokemons })
+      })
+    } else {
+      Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -11,5 +19,6 @@ module.exports = (app) => {
         const message = "La liste des pokémon n'a pas pu être récupérée. Réessayez dans quelques instant.";
         res.status(500).json({message, data: error})
       })
+    }
   });
 }
